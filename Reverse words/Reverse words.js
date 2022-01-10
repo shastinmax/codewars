01141ad3639e5fb70e27fa4341b9a7d42b659149
Shastin Maksim <shastin_maksim@mail.ru>
2022-01-10 20:10:19 +0300

    reverse

---
+++ Reverse words/Reverse words.js
@@ -0,0 +1,6 @@
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+
+function reverseWords(str) {
+    return str.split(' ').map(m => m.split('')).map(m => m.reverse()).map(m => m.join('')).join(' ')
+}
