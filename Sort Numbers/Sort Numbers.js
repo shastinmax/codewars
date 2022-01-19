1493d8458920d41285a8f0e490b6d495cdd3674f
Shastin Maksim <shastin_maksim@mail.ru>
2022-01-19 09:58:51 +0300

    SortNumbers.ts

---
+++ Sort Numbers/Sort Numbers.js
@@ -0,0 +1,7 @@
+// Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+function solution(nums){
+    if(nums===null||nums.length===0){
+        return []
+    }return nums.sort((a,b)=>a-b)
+}
