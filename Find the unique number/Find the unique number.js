ae2ff1a0e40b238c5426774c763aa2f7bf8e287e
Shastin Maksim <shastin_maksim@mail.ru>
2022-01-20 21:10:06 +0300

    Find the unique number

--- Find the unique number/Find the unique number.js
+++ Find the unique number/Find the unique number.js
@@ -1,5 +1,6 @@
 //There is an array with some numbers. All numbers are equal except for one. Try to find it!
 
+
 function findUniq(arr) {
     return +arr.filter((el, i, arrs) => arrs.indexOf(el) === arrs.lastIndexOf(el)).join('')
 }
