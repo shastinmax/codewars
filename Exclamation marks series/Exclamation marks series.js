3faca8069be93b9c36283e9a9ccdc3861c310b16
Shastin Maksim <shastin_maksim@mail.ru>
2022-02-05 09:27:55 +0300

    Exclamation marks series.js

---
+++ Exclamation marks series/Exclamation marks series.js
@@ -0,0 +1,5 @@
+// Description:
+    // Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string. For a beginner kata, you can assume that the input data is always a non empty string, no need to verify it.
+function remove (string) {
+    return `${string.split('').filter(i=>i!=='!').join('')}!`;
+}
