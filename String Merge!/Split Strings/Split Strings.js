7ded39e1f70552690c2b022d64644130f552c0bb
Shastin Maksim <shastin_maksim@mail.ru>
2022-02-11 09:59:06 +0300

    Split Strings.js!

---
+++ String Merge!/Split Strings/Split Strings.js
@@ -0,0 +1,10 @@
+// Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+function solution(str) {
+    let newStr = str.length % 2 === 0 ? str.split('') : (str + '_').split('')
+    let res = []
+    for (let i = 0; i < newStr.length; i += 2) {
+        res.push(newStr[i] + newStr[i + 1])
+    }
+    return res
+}
