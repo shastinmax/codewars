2c207242b8cbe926a848e3424cf88062d84a00d0
Shastin Maksim <shastin_maksim@mail.ru>
2022-02-23 12:49:27 +0300

    Coding Meetup #4

---
+++ Coding Meetup 4/Coding Meetup 4.js
@@ -0,0 +1,12 @@
+// You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising. The list is ordered according to who signed up first.
+//
+//     Your task is to return one of the following strings:
+//
+//     < firstName here >, < country here > of the first Python developer who has signed up; or
+//         There will be no Python developers if no Python developer has signed up.
+//         For example, given the following input array:
+
+function getFirstPython(list) {
+    let arr=list.filter(f=>f.language='Python')
+    return arr.length ? `${arr[0].firstName}, ${arr[0].country}` : 'There will be no Python developers'
+}
