5097bce8e1589431d197281a0d1cb9e8f8567814
Shastin Maksim <shastin_maksim@mail.ru>
2022-04-10 22:21:20 +0300

    Thinkful.js

---
+++ Thinkful/Thinkful.js
@@ -0,0 +1,8 @@
+// Complete the function that takes one argument, a list of words, and returns the length of the longest word in the list.
+//
+//     For example:
+//
+//     ['simple', 'is', 'better', 'than', 'complex'] ==> 7
+function longest(words) {
+    return words.sort((a,b)=>b.length - a.length)[0].length
+}
