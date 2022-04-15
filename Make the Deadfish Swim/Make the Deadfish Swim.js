cc6f7e3beb42478209bdafc084d259c616e5fe21
Shastin Maksim <shastin_maksim@mail.ru>
2022-04-15 21:36:46 +0300

    Make the Deadfish

---
+++ Make the Deadfish Swim/Make the Deadfish Swim.js
@@ -0,0 +1,40 @@
+// Write a simple parser that will parse and run Deadfish.
+//
+//     Deadfish has 4 commands, each 1 character long:
+//
+//     i increments the value (initially 0)
+// d decrements the value
+// s squares the value
+// o outputs the value into the return array
+// Invalid characters should be ignored.
+//
+// parse("iiisdoso") => [ 8, 64 ]
+
+function parse(data) {
+    let arr = []
+    let num = 0
+    for (let i = 0; i < data.length; i++) {
+        switch (data[i]) {
+            case 'i': {
+                num += 1
+                break
+            }
+            case 'd': {
+                num -= 1
+                break
+            }
+            case 's': {
+                num *= num
+                break
+            }
+            case 'o': {
+                arr.push(num)
+                break
+            }
+            default: {
+                break
+            }
+        }
+    }
+    return arr
+}
