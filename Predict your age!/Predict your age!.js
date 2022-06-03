a74601be791005f09702ac6ee10efd222b85fedd
Shastin Maksim <shastin_maksim@mail.ru>
2022-06-03 10:07:48 +0300

    Predict your age!.js

---
+++ Predict your age!/Predict your age!.js
@@ -0,0 +1,12 @@
+// My grandfather always predicted how old people would get, and right before he passed away he revealed his secret!
+//
+//     In honor of my grandfather's memory we will write a function using his formula!
+//
+// Take a list of ages when each of your great-grandparent died.
+//     Multiply each number by itself.
+//     Add them all together.
+//     Take the square root of the result.
+//     Divide by two.
+function predictAge(age1, age2, age3, age4, age5, age6, age7, age8) {
+    return Math.floor(Math.sqrt([...arguments].map(el => el * el).reduce((acc, ell) => acc + ell, 0)) / 2)
+}
