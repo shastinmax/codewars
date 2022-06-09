e1c96b43383c3cee9f4cfaa6fb80043ffa150414
Shastin Maksim <shastin_maksim@mail.ru>
2022-06-09 22:39:36 +0300

    Binary Addition.js

---
+++ Find the Missing Number/Find the Missing Number.js
@@ -0,0 +1,13 @@
+// The following was a question that I received during a technical interview for an entry level software developer position. I thought I'd post it here so that everyone could give it a go:
+//
+// You are given an unsorted array containing all the integers from 0 to 100 inclusively. However, one number is missing. Write a function to find and return this number. What are the time and space complexities of your solution?
+function missingNo(nums) {
+    let num
+    let arr = nums.sort((a, b) => a - b)
+    console.log(nums)
+    for (let i = 0; i <= arr.length; i++) {
+        if (!arr.includes(i)) {
+            return i
+        }
+    }
+}
