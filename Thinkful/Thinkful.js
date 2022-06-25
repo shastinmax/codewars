c58d4db69fda1b2d285a2203ffd6f9c458359deb
Shastin Maksim <shastin_maksim@mail.ru>
2022-06-25 21:17:18 +0300

    By 3

--- Thinkful/Thinkful.js
+++ Thinkful/Thinkful.js
@@ -6,3 +6,18 @@
 function longest(words) {
     return words.sort((a,b)=>b.length - a.length)[0].length
 }
+function fib(n) {
+	if(n ===0 || typeof n !== 'number'){
+		return ''
+	}
+	if(n ===1){
+		return '0'
+	}
+	let arr = [0,1]
+	for (let i= 1; i<n-1;i++){
+		arr.push(arr[i]+arr[i-1])
+	}
+
+	console.log(arr.join(' '))
+}
+fib(3)
