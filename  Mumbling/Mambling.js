acec282c5354ef060d92c709bb13d5352260afd0
Shastin Maksim <shastin_maksim@mail.ru>
2022-07-12 22:59:08 +0300

    Mambling.js

---
+++  Mumbling/Mambling.js
@@ -0,0 +1,9 @@
+// This time no story, no theory. The examples below show you how to write function accum:
+//
+// 	Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+function accum(s) {
+	return s.split('').map(el => el.toUpperCase()).map((el, i) => el + (el.toLowerCase()).repeat(i)).join('-')
+}
