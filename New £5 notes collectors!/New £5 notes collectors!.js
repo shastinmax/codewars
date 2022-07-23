59e8502d746b2a4fcea376187b811e81ef02dc8d
Shastin Maksim <shastin_maksim@mail.ru>
2022-07-23 10:42:57 +0300

    Title Case

--- New £5 notes collectors!/New £5 notes collectors!.js
+++ New £5 notes collectors!/New £5 notes collectors!.js
@@ -3,7 +3,35 @@
 // 	Given a salary and the array of bills, calculate your disposable income for a month and return it as a number of new £5 notes you can get with that amount. If the money you've got (or do not!) doesn't allow you to get any £5 notes return 0.
 //
 // £££ GOOD LUCK! £££
-function getNewNotes(salary,bills){
-	let sum = salary - bills.reduce((a,b)=> a + b, 0);
+function getNewNotes(salary, bills) {
+	let sum = salary - bills.reduce((a, b) => a + b, 0);
 	return sum > 0 ? Math.floor(sum / 5) : 0;
+}
+
+// A string is considered to be in title case if each word in the string is either (a) capitalised (that is, only the first letter of the word is in upper case) or (b) considered to be an exception and put entirely into lower case unless it is the first word, which is always capitalised.
+//
+// 	Write a function that will convert a string into title case, given an optional list of exceptions (minor words). The list of minor words will be given as a string with each word separated by a space. Your function should ignore the case of the minor words string -- it should behave in the same way even if the case of the minor word string is changed.
+function titleCase(title, minorWords) {
+	if (!title) {
+		return ''
+	}
+	if (!minorWords) {
+		title = title.toLowerCase().split(' ')
+		for (let i = 0; i < title.length; i++) {
+			title[i] = title[i][0].toUpperCase() + title[i].slice(1)
+		}
+	}
+	if (minorWords) {
+		minorWords = minorWords.toLowerCase().split(' ')
+		title = title.toLowerCase().split(' ')
+		for (let i = 0; i < title.length; i++) {
+			if (minorWords.includes(title[i]) && i === 0) {
+				title[i] = title[i][0].toUpperCase() + title[i].slice(1)
+			}
+			if (!minorWords.includes(title[i])) {
+				title[i] = title[i][0].toUpperCase() + title[i].slice(1)
+			}
+		}
+	}
+	return title.join(' ')
 }
