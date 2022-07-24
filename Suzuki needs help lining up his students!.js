ccb26aeff5c7b9853196c439d34443eaf910d7b1
Shastin Maksim <shastin_maksim@mail.ru>
2022-07-24 11:23:05 +0300

    Suzuki needs help lining up his students!.js

---
+++ Suzuki needs help lining up his students!.js
@@ -0,0 +1,12 @@
+// Suzuki needs help lining up his students!
+//
+// 	Today Suzuki will be interviewing his students to ensure they are progressing in their training. He decided to schedule the interviews based on the length of the students name in descending order. The students will line up and wait for their turn.
+//
+// 	You will be given a string of student names. Sort them and return a list of names in descending order.
+//
+// 	Here is an example input:
+//
+// 	string = 'Tadashi Takahiro Takao Takashi Takayuki Takehiko Takeo Takeshi Takeshi'
+function lineupStudents(students){
+	return students.trim().split(' ').sort((a, b) => b.length - a.length || b.localeCompare(a))
+}
