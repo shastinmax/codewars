040cc89f24937e4b08f770b3898394e7b9b22125
Shastin Maksim <shastin_maksim@mail.ru>
2022-07-24 15:05:03 +0300

    Suzuki needs help lining up his students!.js

--- Suzuki needs help lining up his students!.js
+++ Suzuki needs help lining up his students!.js
@@ -7,6 +7,27 @@
 // 	Here is an example input:
 //
 // 	string = 'Tadashi Takahiro Takao Takashi Takayuki Takehiko Takeo Takeshi Takeshi'
-function lineupStudents(students){
+function lineupStudents(students) {
 	return students.trim().split(' ').sort((a, b) => b.length - a.length || b.localeCompare(a))
+}
+
+function deleteNth(arr, n) {
+	if (arr.length === 0) {
+		return []
+	}
+	let result = []
+	let res = arr.reduce((acc, el) => {
+		const keys = Object.keys(acc)
+		if (keys.includes(el + '')) {
+			acc[el] += 1
+			if (acc[el] <= n) {
+				result.push(el)
+			}
+		} else {
+			acc[el] = 1
+			result.push(el)
+		}
+		return acc
+	}, {})
+	return result
 }
