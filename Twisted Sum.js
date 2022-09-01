672da305de981214ef310e26eef3529165803768
Shastin Maksim <shastin_maksim@mail.ru>
2022-09-01 12:26:44 +0300

    Integer depth.js

--- Twisted Sum.js
+++ Twisted Sum.js
@@ -12,13 +12,30 @@
 
 function twistedSum(n) {
 	let sum = 0
-	for(let i = 1; i <= n; i++){
-		if((i + '').length ===1){
-			sum+=i
-		}
-		else{
-			sum += (i + '').split('').reduce((acc,el)=>acc + +el,0)
+	for (let i = 1; i <= n; i++) {
+		if ((i + '').length === 1) {
+			sum += i
+		} else {
+			sum += (i + '').split('').reduce((acc, el) => acc + +el, 0)
 		}
 	}
 	return sum;
+}
+
+function computeDepth(x) {
+	let res = [], count = 0, num, i = 1
+
+	do {
+		num = x * i
+		String(num).split('').forEach(el => {
+			if (!res.includes(el)) {
+				res.push(el)
+
+			}
+		})
+		i++
+		count++
+		num = 0
+	} while (res.length < 10)
+	return count
 }
