a9dc38e244e5e83223115d59ec145b62bfdb6028
Shastin Maksim <shastin_maksim@mail.ru>
2022-09-02 16:28:33 +0300

    Length of missing array.js

---
+++ Length of missing array.js
@@ -0,0 +1,42 @@
+// You get an array of arrays.
+// 	If you sort the arrays by their length, you will see, that their length-values are consecutive.
+// 	But one array is missing!
+//
+//
+// 	You have to write a method, that return the length of the missing array.
+//
+// 	Example:
+// [[1, 2], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]] --> 3
+//
+// If the array of arrays is null/nil or empty, the method should return 0.
+//
+// When an array in the array is null or empty, the method should return 0 too!
+// 	There will always be a missing element and its length will be always between the given arrays.
+//
+// 	Have fun coding it and please don't forget to vote and rank this kata! :-)
+//
+// I have created other katas. Have a look if you like coding and challenges.
+
+function getLengthOfMissingArray(arrayOfArrays) {
+	let res = 0
+	if (!arrayOfArrays) {
+		return res
+	}
+	if (arrayOfArrays.includes(null)) {
+		return res
+	}
+	if (arrayOfArrays.length === 0) {
+		return 0
+	}
+	let arr = arrayOfArrays.map(el => el.length).sort((a, b) => a - b)
+
+	for (let i = arr[0]; i <= arr[arr.length - 1]; i++) {
+		if (i === 0) {
+			break
+		}
+		if (!arr.includes(i)) {
+			res = i
+		}
+	}
+	return res
+}
