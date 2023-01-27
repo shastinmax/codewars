a74f19ef7108fc29e1ea1eba40a94b51f70e8d11
Maxim Shastin <shastin_maksim@mail.ru>
2023-01-27 13:54:43 +0300

    Snail crawls up.js

---
+++ Snail crawls up.js
@@ -0,0 +1,28 @@
+// The snail crawls up the column. During the day it crawls up some distance. During the night she sleeps, so she slides down for some distance (less than crawls up during the day).
+//
+// Your function takes three arguments:
+//
+// 	The height of the column (meters)
+// The distance that the snail crawls during the day (meters)
+// Calculate number of day when the snail will reach the top of the column.
+//
+// 	The distance that the snail slides down during the night (meters)
+
+
+	function snail(column, day, night) {
+
+	let sum = 0
+	let count = 0
+
+	for(let i = 0; i < column; i++){
+		if(sum < column){
+			sum += day
+			count += 1
+			if(sum < column){
+				sum -= night
+			}
+		}
+	}
+
+	return count
+}
