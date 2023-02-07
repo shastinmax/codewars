d4ee3953cab046cb60462bbc8ae5a6257b7c5f7f
Maxim Shastin <shastin_maksim@mail.ru>
2023-02-07 23:29:55 +0300

    Leonardo Dicaprio and Oscars.js

---
+++ Leonardo Dicaprio and Oscars.js
@@ -0,0 +1,20 @@
+// if oscar was (integer) 88, you have to return "Leo finally won the oscar! Leo is happy".
+// 	if oscar was 86, you have to return "Not even for Wolf of wallstreet?!"
+// if it was not 88 or 86 (and below 88) you should return "When will you give Leo an Oscar?"
+// if it was over 88 you should return "Leo got one already!"
+
+function leo(oscar){
+	console.log(oscar)
+	if(oscar ===88){
+		return "Leo finally won the oscar! Leo is happy"
+	}
+	if(oscar > 88){
+		return "Leo got one already!"
+	}
+	if(oscar ===87 || oscar < 86){
+		return "When will you give Leo an Oscar?"
+	}
+	if(oscar ===86){
+		return"Not even for Wolf of wallstreet?!"
+	}
+}
